daca9d95e04ada552e66bd8abfdbf8321ac432f4
Emmanuel Iyanu <emmanueliyanu2012@gmail.com>
2020-06-27 21:27:33 +0100

    few changes

---
+++ frontend/src/data.js
@@ -0,0 +1,31 @@
+export default {
+    products: [
+        {
+            name: 'Slim Shirt',
+            category: "Shirt",
+            image: '/images/d1.jpg',
+            price: 60,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10
+        },
+        {
+            name: 'Fit Shirt',
+            category: "Shirt",
+            image: '/images/d1.jpg',
+            price: 80,
+            brand: 'Nike',
+            rating: 4.2,
+            numReviews: 10
+        },
+        {
+            name: 'Best Pant',
+            category: "Pants",
+            image: '/images/d1.jpg',
+            price: 90,
+            brand: 'Nike',
+            rating: 4.4,
+            numReviews: 10
+        },
+    ]
+}
