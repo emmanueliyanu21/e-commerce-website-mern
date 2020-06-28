03a97f9f414008fc539227efd4c7ab9a2ed7a9ff
Emmanuel Iyanu <emmanueliyanu2012@gmail.com>
2020-06-28 11:55:28 +0100

    successfully fetching datas from the redux library

--- frontend/src/Screen/HomeScreen.js
+++ frontend/src/Screen/HomeScreen.js
@@ -1,43 +1,42 @@
-import React, { useState, useEffect } from 'react'
+import React, { useEffect } from 'react'
 import { Link } from 'react-router-dom';
-import axios from 'axios';
+import { listProducts } from '../actions/productActions'
+import { useSelector, useDispatch } from 'react-redux';
 
 const HomeScreen = (props) => {
-    const [products, setProduct] = useState([]);
-    // const productList = useSelector(state => state.productList);
-    // const { products, loading, error } = productList;
-
+    const productList = useSelector(state => state.productList);
+    const { products, loading, error } = productList;
+    const dispatch = useDispatch();
     useEffect(() => {
-        const fetchData = async () => {
-            const { data } = await axios.get('/api/products');
-            setProduct(data);
-        }
-        fetchData();
+        dispatch(listProducts());
         return () => {
             //
-        }
+        };
     }, [])
 
     return (
-        <ul className="products">
-            {
-                products.map(product =>
-                    <li key={product._id}>
-                        <div className="product">
-                            <Link to={'/product/' + product._id}>
-                                <img className="product-image" src={product.image} alt="product" />
-                            </Link>
-                            <div className="product-name">
-                                <Link to={'/product/' + product._id}>{product.name}</Link>
+
+        loading ? (<div> Loading...</div >) : error ? (<div>{error}</div>) : (
+            <ul className="products">
+                {
+                    products.map(product =>
+                        <li key={product._id}>
+                            <div className="product">
+                                <Link to={'/product/' + product._id}>
+                                    <img className="product-image" src={product.image} alt="product" />
+                                </Link>
+                                <div className="product-name">
+                                    <Link to={'/product/' + product._id}>{product.name}</Link>
+                                </div>
+                                <div className="product-brand">{product.brand}</div>
+                                <div className="product-price">${product.price}</div>
+                                <div className="product-rating">{product.rating} Stars ({product.numReviews})</div>
                             </div>
-                            <div className="product-brand">{product.brand}</div>
-                            <div className="product-price">${product.price}</div>
-                            <div className="product-rating">{product.rating} Stars ({product.numReviews})</div>
-                        </div>
-                    </li>
-                )
-            }
-        </ul>
+                        </li>
+                    )
+                }
+            </ul>
+        )
     )
 }
 
